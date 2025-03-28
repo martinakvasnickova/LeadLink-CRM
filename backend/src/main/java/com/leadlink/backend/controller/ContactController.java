758064225954e2a7cd6929a6e0b25d93410595ae
Martina Kvasničková <martinkakvasnickova@gmail.com>
2025-03-28 11:47:09 +0100

    sql

--- backend/src/main/java/com/leadlink/backend/controller/ContactController.java
+++ backend/src/main/java/com/leadlink/backend/controller/ContactController.java
@@ -1,2 +1,29 @@
-package com.leadlink.backend.controller;public class ContactController {
+package com.leadlink.backend.controller;
+
+import com.leadlink.backend.model.Contact;
+import com.leadlink.backend.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ContactController {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    @PostMapping("/contact")
+    Contact newContact(@RequestBody Contact newContact){
+        return contactRepository.save(newContact);
+    }
+
+    @GetMapping("/contacts")
+    List<Contact> getAllContacts(){
+        return contactRepository.findAll();
+    }
+
 }
