83b269b32e9966f602541c4dec32eab57949e8d9
Martina Kvasničková <martinkakvasnickova@gmail.com>
2025-03-28 10:41:55 +0100

    commit

--- backend/src/main/java/com/leadlink/backend/model/Contact.java
+++ backend/src/main/java/com/leadlink/backend/model/Contact.java
@@ -1,2 +1,48 @@
-package com.leadlink.backend.model;public class Contact {
+package com.leadlink.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Contact {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String firstname;
+    private String lastname;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
