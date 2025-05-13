13d9a6718c865efd999c42529c48e84456120e34
Martina Kvasničková <martinkakvasnickova@gmail.com>
2025-05-13 14:10:18 +0200

    openapiconfig

--- backend/src/main/java/com/leadlink/backend/configuration/OpenApiConfig.java
+++ backend/src/main/java/com/leadlink/backend/configuration/OpenApiConfig.java
@@ -1,2 +1,26 @@
-package com.leadlink.backend.configuration;public class OpenApiConfig {
+package com.leadlink.backend.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("My API").version("v1").description("API Documentation"))
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("bearerAuth", new SecurityScheme()
+                                .name("Authorization")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)));
+    }
 }
